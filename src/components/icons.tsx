c960a8bcc3db9ec0bc43fb9b380065a3c49b017e
Alberto Rosas <luisrosasbocanegra@gmail.com>
2026-02-24 18:48:47 +0000

    quiero que pongas como logo la imagen leon.png de la ruta public/images

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 export function Logo({ className }: { className?: string }) {
   return (
     <Image
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Club_Le%C3%B3n_logo.svg/1029px-Club_Le%C3%B3n_logo.svg.png"
+      src="/images/leon.png"
       alt="La Dungeon"
-      width={1029}
-      height={1200}
+      width={500}
+      height={500}
       className={cn(className)}
     />
   );
